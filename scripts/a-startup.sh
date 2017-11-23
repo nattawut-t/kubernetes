77c2dd93f95207b033052432871f49e653d2817d
j2sdk_ball2 <nattawut@moneytable.com>
2017-11-23 07:27:20 +0700

    update service

--- scripts/a-startup.sh
+++ scripts/a-startup.sh
@@ -1,4 +1,4 @@
-eval $(minikube docker-env)
+# eval $(minikube docker-env)
 docker rm -f authen-api
 docker rmi -f authen-api:1.0.0
 docker build -t authen-api:1.0.0 ../authen-api
--- scripts/z-cleanup.sh
+++ scripts/z-cleanup.sh
@@ -1,1 +1,3 @@
-eval $(minikube docker-env -u)
+kubectl delete service authen-api-service
+kubectl delete deployment authen-api-deployment
+# eval $(minikube docker-env -u)
